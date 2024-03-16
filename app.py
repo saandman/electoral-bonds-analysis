8303339bad786822c72e9c201f3dd2141667bb06
saandman <sandeep.email@proton.me>
2024-03-16 21:33:56 +0530

    removed preprocessing. code optimized.

--- app.py
+++ app.py
@@ -1,5 +1,4 @@
 import helper
-import pandas as pd
 import streamlit as st
 
 
@@ -7,18 +6,17 @@ st.sidebar.title('The Electoral Bond Analysis')
 st.sidebar.markdown('Download the cleaned excel sheet from [here](https://gofile.io/d/zMzapV)')
 
 try:
-    df = pd.read_excel('donations.xlsx')
-    df, banana = helper.donation_preprocess(df)  
+    df = helper.load_data('donations_cleaned.csv')
+    df = helper.donor_preprocess(df)
+    banana = helper.banana_preprocess(df)
 except Exception as e:
     st.error(str(e))
 
-# selection
 name_list = helper.fetch_name_list(df)
 selected_name = st.sidebar.selectbox('Select donor', name_list)
 columns_to_display = ['purchase_date', 'donor_name', 'amount']
 
 if st.sidebar.button("Show Analysis"):
-    
     if selected_name == "All":
         donor_stats = helper.fetch_all_donation_stats(df)
         st.title(f'Bonds Issued: ₹{donor_stats.total_donation}')
@@ -41,7 +39,7 @@ if st.sidebar.button("Show Analysis"):
         st.write(banana)
 
         st.markdown(f"##### League")
-        league = helper.get_league_table(banana)
+        league = helper.fetch_league_table(banana)
         st.write(league)
         st.markdown("""
         * count_percentage - percentage of people out of the whole
@@ -60,66 +58,51 @@ if st.sidebar.button("Show Analysis"):
         temp_df = df[df['donor_name'] == selected_name]
         search_url = helper.get_search_url(selected_name)
         st.markdown(f'<a href="{search_url}" target="_blank">Google {selected_name}\'s activities</a>', unsafe_allow_html=True)
-        donor_stats = helper.fetch_user_donation_stats(selected_name, temp_df, banana)
+        user_stats = helper.fetch_user_donation_stats(selected_name, temp_df, banana)
         col1, col2 = st.columns(2)
         with col1:
-            st.markdown(f"##### Date Range: {donor_stats.start_year} - {donor_stats.end_year}")
-            st.markdown(f"##### League: {donor_stats.rating}")
-            st.markdown(f"##### No. of Donations: {donor_stats.num_donations}")
-            st.markdown(f"##### Total Donation: ₹{donor_stats.total_donation}")
-            # st.markdown(f"##### (in words: {helper.convert_to_indian_currency(donor_stats.total_donation)})")
-
+            st.markdown(f"##### Date Range: {user_stats.start_year} - {user_stats.end_year}")
+            st.markdown(f"##### League: {user_stats.rating}")
+            st.markdown(f"##### No. of Donations: {user_stats.num_donations}")
+            st.markdown(f"##### Total Donation: ₹{user_stats.total_donation}")
         with col2:
-            st.markdown(f"##### Mean/Avg Donation: ₹{donor_stats.mean_donation}")
-            st.markdown(f"##### Median Donation: ₹{donor_stats.median_donation}")
-            st.markdown(f"##### Most Frequent denomination: ₹{donor_stats.freq_donation_denominations}")
+            st.markdown(f"##### Mean/Avg Donation: ₹{user_stats.mean_donation}")
+            st.markdown(f"##### Median Donation: ₹{user_stats.median_donation}")
+            st.markdown(f"##### Most Frequent denomination: ₹{user_stats.freq_donation_denominations} ({user_stats.max_freq_donation_count} times)")
         st.write(temp_df[columns_to_display])
         helper.plot_purchase_heatmap(temp_df)
         
     
     if selected_name == "All":
-        try:
-            encashment = pd.read_excel('encashments.xlsx')
-            encashment, redemptions = helper.encashment_preprocess(encashment)
-        except Exception as e:
-            st.error(str(e))
+        encashment = helper.load_data('encashment_cleaned.csv')
         st.markdown(f"## Party Stats")
         encashment_stats = helper.fetch_encashment_stats(encashment)
+        redemptions = helper.redemptions_preprocess(encashment, encashment_stats.total_encashed)
         st.markdown(f"##### Number of Parties: {encashment_stats.num_parties}")
         st.markdown(f"##### Number of times Bonds Encashed: {encashment_stats.num_encashments}")
         st.markdown(f"##### Total Encashed: ₹{encashment_stats.total_encashed}")
 
-        st.write(redemptions)
-
     if selected_name == "All":
-        helper.plot_purchase_encashed_heatmaps(df, encashment)
-        encashed_minus_issued = df.amount.sum() - redemptions.total_amount.sum()
-        # amount_in_words = helper.convert_to_indian_currency(encashed_minus_issued)
-
-        # st.markdown(f"""There is a discrepancy of **Rs. {encashed_minus_issued} ({amount_in_words})** in the difference of EB encashed from EB issued. From the graph below, it looks like some data of EB issued data in April 2019 is missing, but is encashed, could someone else confirm?""")
-        st.markdown(f"""There is a discrepancy of **Rs. {encashed_minus_issued}** in the difference of EB encashed from EB issued. From the graph below, it looks like some data of EB issued data in April 2019 is missing, but is encashed, could someone else confirm?""")
+        helper.plot_purchase_encashed_heatmap(df, encashment)
+        encashed_minus_issued = donor_stats.total_donation - encashment_stats.total_encashed
+        st.markdown(f"""There is a discrepancy of **Rs. {encashed_minus_issued}** in the EB encashed from EB issued data given.""")
         st.markdown(f"""(Graphs not in order, check year)""")
         helper.plot_encashment(df, encashment)
         helper.plot_purchase_vs_encashment(donor_stats.total_donation, encashment_stats.total_encashed)
-
 
 
     if selected_name == "All":
         st.markdown(f"## Wordcloud of Donors")
         wordcloud_image = helper.generate_wordcloud(df)
         st.image(wordcloud_image, use_column_width=True)
-        # donor_stats = helper.generate_wordcloud(df)
 
 
 st.sidebar.markdown("""---""") 
+st.sidebar.markdown("""---""")
+st.sidebar.markdown("""---""")
 st.sidebar.markdown("""
-    Support an atmanirbhar dev today -
+    Support an atmanirbhar devoloper today -
     * UPI - sandman.zip@oksbi
     * [PayPal](https://paypal.me/saandman)
     * [Buy Me A Coffee](https://www.buymeacoffee.com/saandman)
 """)
-st.sidebar.markdown("""
-    For enquiries - [email](mailto:sandman.email@proton.me)
-""")
-st.sidebar.markdown("Thank you, have a nice day :)")
-st.sidebar.markdown("""---""")
--- helper.py
+++ helper.py
@@ -1,95 +1,59 @@
 import pandas as pd
-import numpy as np
 import streamlit as st
 import matplotlib.pyplot as plt
 from collections import namedtuple
-from num2words import num2words
 import seaborn as sns
 import calendar
 from wordcloud import WordCloud
 import io
-import re
 
 
 ############# Preprocess ################
 
-def clean_date(date_str):
-    try:
-        return pd.to_datetime(date_str, format="%d/%b/%Y")
-    except Exception as e:
-        return pd.NaT
 
-def clean_amount(amount):
-    try:
-        if isinstance(amount, str):
-            amount = amount.replace(',', '').replace(' ', '')
-            amount = np.int64(amount)
-            return amount
-    except (ValueError, OverflowError):
-        return np.nan
+@st.cache_resource
+def load_data(file_path):
+    return pd.read_csv(file_path)
 
-def clean_name(name):
-    try:
-        cleaned_name = re.sub(r'\s+', ' ', name).strip()
-        return cleaned_name
-    except Exception as e:
-        return np.nan
+def donor_preprocess(df):
+    df['purchase_date'] = pd.to_datetime(df['purchase_date'])
+    return df
+
+def encashment_preprocess(encashment):
+    encashment['encashment_date'] = pd.to_datetime(encashment['encashment_date'])
+    return encashment
+
+BANANA_EMOJI = {
+    '🍌': 100000,
+    '🍌🍌': 5000000,
+    '🍌🍌🍌': 10000000,
+    '🍌🍌🍌🍌': 500000000
+}
 
 def banana_rating(total_donation):
-    if total_donation < 100000:
-        return '🍌'
-    elif 100000 <= total_donation < 5000000:
-        return '🍌🍌'
-    elif 5000000 <= total_donation < 10000000:
-        return '🍌🍌🍌'
-    elif 10000000 <= total_donation < 500000000:
-        return '🍌🍌🍌🍌'
-    else:
-        return '🍌🍌🍌🍌🍌'
+    for rating, threshold in BANANA_EMOJI.items():
+        if total_donation < threshold:
+            return rating
+    return '🍌🍌🍌🍌🍌'
     
-
-def donation_preprocess(df):
-    df.rename(columns={'Date of Purchase': 'purchase_date',
-                        'Purchaser Name': 'donor_name',
-                        'Denomination': 'amount'}, inplace=True)
-    df.dropna(how='all', inplace=True, ignore_index=True)
-    df['purchase_date'] = df['purchase_date'].apply(clean_date)
-    df['amount'] = df['amount'].apply(clean_amount)
-    df['donor_name'] = df['donor_name'].apply(clean_name)
-    df['year'] = df['purchase_date'].dt.year
-    df['year'] = df['year'].astype(int)
-    df['month'] = df['purchase_date'].dt.month
-
+def banana_preprocess(df):
     banana = df.groupby('donor_name').agg({'amount': 'sum'}).reset_index()
     banana['banana_rating'] = banana['amount'].apply(banana_rating)
     banana = banana.sort_values(by='amount', ascending=False)
+    return banana
 
-    return df, banana
-
-def encashment_preprocess(encashment):
-    encashment.rename(columns={'Date of\nEncashment': 'encashment_date',
-                        'Name of the Political Party': 'political_party',
-                        'Denomination': 'amount'}, inplace=True)
-    encashment.dropna(how='all', inplace=True, ignore_index=True)
-    encashment['encashment_date'] = encashment['encashment_date'].apply(clean_date)
-    encashment['amount'] = encashment['amount'].apply(clean_amount)
-    encashment['political_party'] = encashment['political_party'].apply(clean_name)
-    encashment['year'] = encashment['encashment_date'].dt.year
-    encashment['year'] = encashment['year'].astype(int)
-    encashment['month'] = encashment['encashment_date'].dt.month
-
-
+def redemptions_preprocess(encashment, total_encashed):
     redemptions = encashment.political_party.value_counts()
     encashment_by_party = encashment.groupby('political_party').agg({'amount': 'sum'}).reset_index()
     redemptions = pd.merge(redemptions, encashment_by_party, on='political_party', how='left')
     redemptions.rename(columns={'count': 'count_of_encashments',
                         'amount': 'total_amount'}, inplace=True)
-    total_amount = encashment['amount'].sum()
-    redemptions['percentage_share'] = (redemptions['total_amount'] / total_amount) * 100
+    redemptions['percentage_share'] = (redemptions['total_amount'] / total_encashed) * 100
+    return redemptions
 
-    return encashment, redemptions
 
 ################### Helper functions ####################
+
 
 def fetch_name_list(df):
     name_list = df['donor_name'].unique().tolist()
@@ -99,57 +63,62 @@ def fetch_name_list(df):
 
 def fetch_all_donation_stats(df):
     num_donations = df.shape[0]
-    num_donors = len(df['donor_name'].unique())
-    donations_denominations = df.amount.unique()
+    num_donors = df['donor_name'].nunique()
+    donations_denominations = df['amount'].unique()
     unique_donation_denominations = f"{len(donations_denominations)} - {donations_denominations}"
-    freq_donation_denominations = df['amount'].mode().iloc[0]
-    max_freq_donation_count = df['amount'].value_counts().loc[freq_donation_denominations]
+    amount_counts = df['amount'].value_counts()
+    freq_donation_denominations = amount_counts.idxmax()
+    max_freq_donation_count = amount_counts.max()
     mean_donation = df['amount'].mean().round(2)
     median_donation = df['amount'].median().round(2)
     total_donation = df['amount'].sum().round(2)
     start_year = df['purchase_date'].dt.year.min().astype(int)
     end_year = df['purchase_date'].dt.year.max().astype(int)
 
-    Stats = namedtuple('Stats', ['num_donations', 'num_donors', 'unique_donation_denominations', 
+    DonationStats = namedtuple('Stats', ['num_donations', 'num_donors', 'unique_donation_denominations', 
                                 'freq_donation_denominations', 'max_freq_donation_count', 'mean_donation', 'median_donation', 
                                 'total_donation', 'start_year', 'end_year',])
-    stats_instance = Stats(num_donations=num_donations, num_donors=num_donors, unique_donation_denominations=unique_donation_denominations, 
+    donor_stats = DonationStats(num_donations=num_donations, num_donors=num_donors, unique_donation_denominations=unique_donation_denominations, 
                            freq_donation_denominations=freq_donation_denominations, max_freq_donation_count=max_freq_donation_count, mean_donation=mean_donation,
                            median_donation=median_donation, total_donation=total_donation, start_year=start_year, end_year=end_year)
-    return stats_instance
+    return donor_stats
 
 
 def fetch_user_donation_stats(selected_name, df, banana):
     rating = banana.loc[banana['donor_name'] == selected_name, 'banana_rating'].values[0]
     num_donations = df.shape[0]
-    freq_donation_denominations = df['amount'].mode().iloc[0]
+
+    amount_counts = df['amount'].value_counts()
+    freq_donation_denominations = amount_counts.idxmax()
+    max_freq_donation_count = amount_counts.max()
+
     mean_donation = df['amount'].mean().round(2)
     median_donation = df['amount'].median().round(2)
     total_donation = df['amount'].sum().round(2)
     start_year = df['purchase_date'].dt.year.min().astype(int)
     end_year = df['purchase_date'].dt.year.max().astype(int)
 
-    Stats = namedtuple('Stats', ['rating', 'num_donations', 'freq_donation_denominations', 
+    UserStats = namedtuple('Stats', ['rating', 'num_donations', 'freq_donation_denominations', 'max_freq_donation_count',
                                  'mean_donation', 'median_donation', 'total_donation', 'start_year', 'end_year',])
     
-    stats_instance = Stats(rating=rating, num_donations=num_donations, 
-                           freq_donation_denominations=freq_donation_denominations, mean_donation=mean_donation,
-                           median_donation=median_donation, total_donation=total_donation,
+    user_stats = UserStats(rating=rating, num_donations=num_donations, 
+                           freq_donation_denominations=freq_donation_denominations, max_freq_donation_count=max_freq_donation_count,
+                           mean_donation=mean_donation, median_donation=median_donation, total_donation=total_donation,
                            start_year=start_year, end_year=end_year)
-    return stats_instance
+    return user_stats
 
 
 def fetch_encashment_stats(encashment):
     num_encashments = encashment.shape[0]
-    num_parties = len(encashment['political_party'].unique())
-    total_encashed = encashment['amount'].sum().round(2)
+    num_parties = encashment['political_party'].nunique()
+    total_encashed = encashment['amount'].sum()
 
-    Stats = namedtuple('Stats', ['num_encashments', 'num_parties', 'total_encashed'])
-    stats_instance = Stats(num_encashments=num_encashments, num_parties=num_parties, 
+    EncashmentStats = namedtuple('Stats', ['num_encashments', 'num_parties', 'total_encashed'])
+    encashment_stats = EncashmentStats(num_encashments=num_encashments, num_parties=num_parties, 
                            total_encashed=total_encashed)
-    return stats_instance
+    return encashment_stats
 
-def get_league_table(banana):
+def fetch_league_table(banana):
     league = banana['banana_rating'].value_counts().reset_index(name='count')
     amount_sum = banana.groupby('banana_rating')['amount'].sum().reset_index(name='sum_amount')
     league = pd.merge(league, amount_sum, on='banana_rating')
@@ -159,29 +128,30 @@ def get_league_table(banana):
     league['sum_amount_percentage'] = (league['sum_amount'] / total_sum_amount) * 100
     league['count_percentage'] = league['count_percentage']
     league['sum_amount_percentage'] = league['sum_amount_percentage']
-    # league = league.sort_values(by='banana_rating', key=lambda x: x.str.len())
     league = league.sort_values(by='banana_rating', key=lambda x: x.astype(str).str.len())
-
     return league
+
+def get_search_url(selected_name):
+    # search_url = f"https://www.google.com/search?q={selected_name}+(ED|IT|raid|scam|stocks|Enforcement Directorate|Income Tax)"
+    search_url = f"https://www.google.com/search?q={selected_name}"
+    return search_url
+
+
+################### Plots ####################
+
 
 @st.cache_resource
 def plot_encashment(df, encashment):
     df_grouped = df.groupby(['year', 'month'])['amount'].sum().reset_index()
     encashment_grouped = encashment.groupby(['year', 'month'])['amount'].sum().reset_index()
-
     df_grouped = df_grouped.sort_values(['year', 'month'])
     encashment_grouped = encashment_grouped.sort_values(['year', 'month'])
-
     years = df['year'].unique()
-
     fig, axes = plt.subplots(nrows=len(years), ncols=1, figsize=(10, 6 * len(years)))
-
     for i, year in enumerate(years):
         df_year = df_grouped[df_grouped['year'] == year]
         encashment_year = encashment_grouped[encashment_grouped['year'] == year]
-
         merged_df = pd.merge(df_year, encashment_year, on='month', suffixes=('_purchase', '_encashed'), how='outer')
-
         axes[i].bar(merged_df['month'], merged_df['amount_purchase'], color='green', width=0.4, label='EB Issued')
         axes[i].bar(merged_df['month'] + 0.4, merged_df['amount_encashed'], color='red', width=0.4, label='EB Encashed')
         axes[i].set_xlabel('Month')
@@ -191,64 +161,31 @@ def plot_encashment(df, encashment):
         axes[i].set_xticklabels(['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'])
         axes[i].legend()
         axes[i].ticklabel_format(style='plain', axis='y')
-
     plt.tight_layout()
     st.pyplot(fig)
 
-def plot_date_volume(df):
-    df['purchase_date'] = pd.to_datetime(df['purchase_date'])
-    df['day'] = df['purchase_date'].dt.day
-    plt.figure(figsize=(12, 6))
-    sns.countplot(x=df['day'], data=df.sort_values('purchase_date'))
-    plt.title('Distribution of Volume of Transaction by Date Over Time')
-    plt.xlabel('Day of the month')
-    plt.ylabel('Transaction Count')
-    st.pyplot(plt)
-
 def plot_heatmap(df, title, ax, color='YlGnBu'):
     pivot_table = df.pivot_table(index='year', columns='month', aggfunc='size')
-    # month no. to names
     pivot_table.columns = pivot_table.columns.map(lambda x: calendar.month_name[x])
-    
     sns.heatmap(pivot_table, cmap=color, annot=True, fmt='g', cbar=True, ax=ax)
     ax.set_title(title)
     ax.set_xlabel('Month')
     ax.set_ylabel('Year')
     ax.tick_params(axis='x', rotation=45)
 
-def plot_purchase_encashed_heatmaps(purchase_df, encashed_df):
+@st.cache_resource
+def plot_purchase_encashed_heatmap(purchase_df, encashed_df):
     fig, axes = plt.subplots(1, 2, figsize=(20, 8))
     plot_heatmap(purchase_df, 'Electoral Bonds Purchase Heatmap', axes[0], color='Greens')
     plot_heatmap(encashed_df, 'Electoral Bonds Encashment Heatmap', axes[1], color='Reds')
     st.pyplot(plt)
 
+@st.cache_resource
 def plot_purchase_heatmap(purchase_df):
     plt.figure(figsize=(12, 8))
     ax = plt.gca()
     plot_heatmap(purchase_df, 'Electoral Bonds Purchase Heatmap', ax, color='Greens')
     st.pyplot(plt)
-
-
-def convert_to_indian_currency(number):
-    return num2words(number, lang='en_IN')
-
-def get_search_url(selected_name):
-    search_url = f"https://www.google.com/search?q={selected_name}+(ED|IT|raid|scam|stocks|Enforcement Directorate|Income Tax)"
-    return search_url
-
-
-@st.cache_resource
-def generate_wordcloud(df):
-    text = ' '.join(df['donor_name'])
-    text = text.upper()
-    wordcloud = WordCloud(width=800, height=400, background_color='white').generate(text)
-    
-    # Save wordcloud to a BytesIO object
-    img_buffer = io.BytesIO()
-    wordcloud.to_image().save(img_buffer, format='PNG')
-    
-    return img_buffer.getvalue()
-
 
 @st.cache_resource
 def plot_purchase_vs_encashment(donated, encashed):
@@ -259,5 +196,13 @@ def plot_purchase_vs_encashment(donated, encashed):
     plt.ylabel('Rupees')
     plt.title('In Summary')
     plt.ticklabel_format(style='plain', axis='y')
-    
     st.pyplot(plt)
+
+@st.cache_resource
+def generate_wordcloud(df):
+    text = ' '.join(df['donor_name'])
+    text = text.upper()
+    wordcloud = WordCloud(width=800, height=400, background_color='white').generate(text)
+    img_buffer = io.BytesIO()
+    wordcloud.to_image().save(img_buffer, format='PNG')
+    return img_buffer.getvalue()
